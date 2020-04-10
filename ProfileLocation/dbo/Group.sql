68c1539e14950cdfea85e2ca159553a3df46677f
Victor Saly <Victor.Saly@storm-technologies.com>
2020-04-10 19:48:28 +0100

    feat: add slug and make group names unique

--- ProfileLocation/dbo/Group.sql
+++ ProfileLocation/dbo/Group.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Group]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
-	[Name] [nvarchar](250) NOT NULL,
+	[Name] [nvarchar](250) NOT NULL UNIQUE,
+	[Slug] [nvarchar](250) NOT NULL UNIQUE,
 	[Description] [nvarchar](max) NULL,
 	[Created] [datetime] NOT NULL DEFAULT getdate(),
 	[IsDeleted] [bit] NOT NULL DEFAULT 0,
