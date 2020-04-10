fea7020ca08443d58bde81429e2b544ff68ae95b
Victor Saly <Victor.Saly@storm-technologies.com>
2020-04-10 12:54:13 +0100

    feat: postdeployment plan

---
+++ ProfileLocation/PostDeployment/LookupTables/AddSocialTypes.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO [dbo].[SocialType]
+           ([Name]
+           ,[iconURL])
+     VALUES
+           ('Facebook' ,'https://cdn.pixabay.com/photo/2017/06/22/06/22/facebook-2429746_960_720.png')
+INSERT INTO [dbo].[SocialType]
+           ([Name]
+           ,[iconURL])
+     VALUES
+           ('Instagram' ,'https://cdn.pixabay.com/photo/2017/06/23/02/32/instagram-2433265_960_720.png')
