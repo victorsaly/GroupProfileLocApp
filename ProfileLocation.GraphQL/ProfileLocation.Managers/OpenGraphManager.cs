623a1930d596b91dbf8b1ad813c6f49664dc410e
Victor Saly <Victor.Saly@storm-technologies.com>
2020-04-12 01:46:35 +0100

    feat: add openGraph endpoint to retrieve profile image

---
+++ ProfileLocation.GraphQL/ProfileLocation.Managers/OpenGraphManager.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="OpenGraphManager.cs" company="Victor Saly Ltd">
+// Copyright (c) Victor Saly Ltd. All rights reserved.
+// </copyright>
+
+using OpenGraphNet;
+using ProfileLocation.Domain.Managers.Output;
+using ProfileLocation.Domain.ORM.Attributes;
+
+namespace ProfileLocation.Managers
+{
+    /// <summary>
+    /// OpenGraph Manager to retrive data from website.
+    /// </summary>
+    public class OpenGraphManager : IOpenGraphManager
+    {
+        /// <inheritdoc/>
+        [QLQuery]
+        public OpenGraphProfileOutput GetOpenGraph(string url)
+        {
+            var result = OpenGraph.ParseUrl(url);
+
+            return new OpenGraphProfileOutput()
+            {
+                Image = result?.Image?.ToString(),
+                Title = result?.Title,
+            };
+        }
+    }
+}
